699ca34fbdc2a464b83d583fad377e4df5f80aaa
Vitaly <vivlgud@mail.ru>
2022-11-11 13:45:08 +0200

    input data

---
+++ Task/Program.cs
@@ -0,0 +1,10 @@
+﻿// Из имеющегося массива строк сформировать массив из строк, 
+//длина которых меньше или равна 3 символа
+
+Console.Clear();
+
+string[] arrWord = { "Ckeck", "your", "new", "knowledges", ":)", "!" };
+string[] arrNew = new string[arrWord.Length];
+
+string word = " ";
+int count = 0;
